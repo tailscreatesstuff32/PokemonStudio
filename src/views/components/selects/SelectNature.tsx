85b28041fabddf8a0a8aa88086942386118ebc50
Palbolsky <palbolsky@gmail.com>
2024-09-18 18:42:13 +0200

    Update dependencies, fix TFunction, other fixes

--- src/views/components/selects/SelectNature.tsx
+++ src/views/components/selects/SelectNature.tsx
@@ -1,7 +1,8 @@
 import React, { useMemo } from 'react';
 import { SelectOption } from '@components/SelectCustom/SelectCustomPropsInterface';
 import { State, useGlobalState } from '@src/GlobalStateProvider';
-import { TFunction, useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
+import { TFunction } from 'i18next';
 import { SelectDataProps } from './SelectDataProps';
 import { getText } from '@utils/ReadingProjectText';
 import { SelectCustom } from '@components/SelectCustom';
